b52dbe47a8d813f690b9a2fee7add3dca016e6e2
veraxrl <vera.xu.iforc@gmail.com>
2016-07-18 16:03:21 +0300

    control flow

--- script.js
+++ script.js
@@ -12,32 +12,32 @@ app.config(function($routeProvider) {
 });
 
 app.controller('mainCtrl', function($scope, $http, $firebaseObject, $firebaseArray,$firebaseAuth) {
-	// var ref = firebase.database().ref().child("msgs");
-	// $scope.msgs= $firebaseArray(ref);
+
 
 	SC.initialize({
 	  client_id: 'eb60efff116075efdaa769b3eec7a5f8'
-	  // redirect_uri: 'http://localhost:8000/#/login'
 	});
 
-	SC.stream('/tracks/244261890').then(function(player){
-	  player.play();
-	  console.log(player);
-	  $scope.myPlayer= player.streamInfo;
-	});
-
-	// var tracks = [{id: 290}, {id: 291}, {id: 292}];
-
-	// SC.connect().then(function() {
-	//   SC.post('/playlists', {
-	//     playlist: { title: 'Liked Tracks', tracks: tracks }
-	//   });
-	// });
-
-	// console.log(playlist);
-
-	//<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/244261890&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-
+	if ($scope.trackid==="misty") {
+		SC.stream('/tracks/244261890').then(function(player){
+		  player.play();
+		  console.log(player);
+		  $scope.myPlayer= player.streamInfo;
+		});
+	} else if ($scope.trackid==="latenight") {
+		SC.stream('/tracks/11711484').then(function(player){
+		  player.play();
+		  console.log(player);
+		  $scope.myPlayer= player.streamInfo;
+		});
+	} else if ($scope.trackid==="river") {
+		SC.stream('/tracks/128905480').then(function(player){
+		  player.play();
+		  console.log(player);
+		  $scope.myPlayer= player.streamInfo;
+		});
+	}
+ 
 });
 
 app.controller('LoginCtrl', function($scope, $routeParams, $firebaseObject, $firebaseAuth) {
