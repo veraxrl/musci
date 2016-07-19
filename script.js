aa75d65e552cb04b69ae03f44be2b307831a5b96
veraxrl <vera.xu.iforc@gmail.com>
2016-07-20 01:27:10 +0300

    merged controls todo list

--- script.js
+++ script.js
@@ -12,18 +12,29 @@ app.config(function($routeProvider) {
 });
 
 app.controller('mainCtrl', function($scope, $http, $firebaseArray, $firebaseAuth) {
-	var ref = firebase.database().ref().child("tracks");
-	$scope.tracks= $firebaseArray(ref);
+
 
 	//Function to add new track:
-	$scope.addTrack = function(myName,Id) {
+	$scope.addSCTrack = function(myName,Id) {
+		var ref = firebase.database().ref().child("tracks").child("SoundCloud");
+		$scope.tracks= $firebaseArray(ref);
 	    $scope.tracks.$add({
 	    	name: myName,
 	    	id: Id
 	    });
   	};
 
-  	//Function to play track, triggered by ng-click:
+  	//Function to add Youtube track:
+  	$scope.addYTTrack = function(myName,Id) {
+		var ref = firebase.database().ref().child("tracks").child("Youtube");
+		$scope.tracks= $firebaseArray(ref);
+	    $scope.tracks.$add({
+	    	name: myName,
+	    	id: Id
+	    });
+  	};
+
+  	//Function to select track, triggered by ng-click:
 	$scope.playTrack = function() {
 		SC.initialize({
 		  client_id: 'eb60efff116075efdaa769b3eec7a5f8'
@@ -39,13 +50,14 @@ app.controller('mainCtrl', function($scope, $http, $firebaseArray, $firebaseAuth
 			  $scope.currentPlayer = player;
 			});
 		} else if ($scope.trackid==="clouds") {
-			$scope.anotherGoodOne = 'https://www.youtube.com/watch?v=EhC1K6KCm90';
+			$scope.anotherGoodOne = 'https://www.youtube.com/watch?v='+'EhC1K6KCm90';
 		} else if ($scope.trackid==="electric") {
-			$scope.anotherGoodOne = 'https://www.youtube.com/watch?v=Rv_a6rlRjZk';
+			$scope.anotherGoodOne = 'https://www.youtube.com/watch?v='+'Rv_a6rlRjZk';
 		}
 
 	}
 
+	//Function to control track: (play, pause, stop)
 	$scope.pausePlayer = function() {
 		if ($scope.bestPlayer) $scope.bestPlayer.pauseVideo();
 		if ($scope.currentPlayer) $scope.currentPlayer.pause();
@@ -62,7 +74,8 @@ app.controller('mainCtrl', function($scope, $http, $firebaseArray, $firebaseAuth
 	}
 
 	// Use the below to add new track: 
-	// $scope.addTrack("misty","244261890");
+	//$scope.addSCTrack("river","128905480");
+	//$scope.addYTTrack("clouds","EhC1K6KCm90");
 
 	//Auto play Function:
 	// $scope.$on('youtube.player.ended', function ($event, player) {
@@ -90,3 +103,19 @@ app.controller('LoginCtrl', function($scope, $routeParams, $firebaseObject, $fir
     }
 });
 
+
+//TO-DOS: 
+//1.  Figure out how to do Bootstrap dropdown bar
+//2.  Display all songs in Firebase database in a playlist (CSS work, make it pretty) 
+// The firebase function is already set up. Use function to add track infos. 
+
+//3.  Use firebase db to store songs, their ids. (done!) 
+//4.  Systematially choose the songs to play. (not hardcode if else statement, based on firebase db and user input)
+
+//5.  Instead of manually type in song name, have a dropdown bar to display all songs in the database 
+
+//Future goals: 
+//6.  Figure out how to add songs to database based on URL (String manipulation, to be able to recognize that the 
+//URL belongs to a specific API)
+//7.  How to do chrome extension 
+//8.  How to drag and drop songs 
